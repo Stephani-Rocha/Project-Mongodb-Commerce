8bb2d2ad352ac0060f0d9cd87a6ec4f95b7c99dd
Stephani Rocha <gomes.stephani19@gmail.com>
2022-10-25 12:01:39 -0300

    fix(Correção): Corrige query do requisito 10.

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,3 @@
 db.produtos.find({
     valoresNutricionais: 
-    { $elemMatch: { tipo: "calorias", proteínas: { $lte: 30, $gte: 40 } } } }, { nome: 1, _id: 0 });
+    { $elemMatch: { tipo: "calorias", proteínas: { $gte: 30, $lte: 40 } } } }, { nome: 1, _id: 0 });
